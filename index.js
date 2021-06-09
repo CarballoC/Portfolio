5f89c919ea3cbc2bdc7705d7c6f4f7fa45d65a03
Yoandris Carballo <uscarballo@gmail.com>
2021-06-09 16:08:37 -0700

    First commit Portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+// const logo = document.querySelector("#logo");
+// const hero = document.querySelector(".hero");
+
+// hero.addEventListener("mouseover", runEvent);
+// hero.addEventListener("mouseout", runEvent2);
+
+// function runEvent(e) {
+//   hero.style.background = "#000";
+// }
+// function runEvent2(e) {
+//   hero.style.background = "#171629";
+// }
