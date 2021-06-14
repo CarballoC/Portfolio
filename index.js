dc29901c866243fefa7958f99ba968253e334dd2
Yoandris Carballo <uscarballo@gmail.com>
2021-06-13 20:58:41 -0700

    animation in hero section

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-// const logo = document.querySelector("#logo");
-// const hero = document.querySelector(".hero");
-
-// hero.addEventListener("mouseover", runEvent);
-// hero.addEventListener("mouseout", runEvent2);
-
-// function runEvent(e) {
-//   hero.style.background = "#000";
-// }
-// function runEvent2(e) {
-//   hero.style.background = "#171629";
-// }
+class typeWriter {
+  constructor(txtElement, words, wait = 2000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+  type() {
+    const current = this.wordIndex % this.words.length;
+    const fullTxt = this.words[current];
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.txtElement.innerHTML = `<span classs="txt">${this.txt}</span>`;
+    let typeSpeed = 400;
+    if (this.isDeleting) {
+      typeSpeed /= 3;
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.wordIndex++;
+      typeSpeed = 400;
+    }
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  new typeWriter(txtElement, words, wait);
+}
